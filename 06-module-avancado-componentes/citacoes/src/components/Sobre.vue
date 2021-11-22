a55b14b732c2d8bc7f44a432451a14ccf945d19e
gleissonneves <gleissonneves2017@gmail.com>
2021-11-22 02:16:19 -0300

    feat: new modules

---
+++ 06-module-avancado-componentes/citacoes/src/components/Sobre.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p class="nome">Citações</p>
+        <p class="versao">Versão 1.0</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+	      margin-top: 20px;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .nome {
+        font-size: 1rem;
+        font-weight: bold;
+    }
+
+    .versao {
+        font-size: .75rem;
+    }
+</style>
